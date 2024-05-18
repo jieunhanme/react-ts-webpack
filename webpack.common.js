2b1f38d048834fb3caa4bffca3d25427ac3ef20b
jieunhanme <jieun93.h@gmail.com>
2024-05-19 00:22:35 +0900

    chore: init react

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,36 @@
 import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 /** @type {import('webpack').Configuration} */
 export default {
-  entry: "./index.html",
+  devtool: "cheap-module-source-map",
+  entry: path.resolve(process.cwd(), "/src/index.jsx"),
   output: {
     filename: "main.js",
     path: path.resolve(process.cwd(), "dist"),
   },
+  devServer: {
+    hot: true, // hot reloading
+    port: 3000, // port on which server will run
+    open: true, // open browser automatically on start
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "index.html" }),
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+  ],
 };
